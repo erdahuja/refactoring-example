e82da1d7275effa86d3776bb5af37913605a24d1
Deepak <er.d.ahuja@gmail.com>
2019-03-17 14:43:21 +0530

    parameter dispatch

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-function statement(customer, movies) {
-  let totalAmount = 0;
-  let frequentRenterPoints = 0;
-  let result = `Rental Record for ${customer.name}\n`;
-  for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
+function statement(customer, movies, format = 'text') {
+  switch (format) {
+    case "text":
+      return textStatement();
+    case "html":
+      return htmlStatement();
+  }
+  throw new Error(`unknown statement format ${format}`);
+  function htmlStatement() {
+    let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n`;
+    result += "<table>\n";
+    for (let r of customer.rentals) {
+      result += `  <tr><td>${movieFor(r).title}</td><td>${amountFor(r)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${totalAmount()}</em></p>\n`;
+    result += `<p>You earned <em>${totalFrequentRenterPoints()}</em> frequent renter points</p>\n`;
+    return result;
+  }
+  function textStatement() {
+    let result = `Rental Record for ${customer.name}\n`;
+    for (let r of customer.rentals) {
+      result += `\t${getMovie(r).title}\t${calculateRent(r)}\n`;
+    }
+    result += `Amount owed is ${totalRent()}\n`;
+    result += `You earned ${totalPtrs()} frequent renter points\n`;
+    console.log(result)
+    return result;
+  }
+  function getMovie(r) {
+    return movies[r.movieID];
+  }
+  function calculateRent(r) {
     let thisAmount = 0;
-
-    switch (movie.code) {
+    // determine amount for each movie
+    switch (getMovie(r).code) {
       case "regular":
         thisAmount = 2;
         if (r.days > 2) {
@@ -23,15 +50,37 @@ function statement(customer, movies) {
         }
         break;
     }
-
-    frequentRenterPoints++;
-    if (movie.code === "new" && r.days > 2) frequentRenterPoints++;
-
-    result += `\t${movie.title}\t${thisAmount}\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
-
-  return result;
+  function totalRent() {
+    let totalAmount = 0;
+    for (let r of customer.rentals) {
+      totalAmount += calculateRent(r);
+    }
+    return totalAmount;
+  }
+  function totalPtrs() {
+    let frequentRenterPoints = 0;
+    for (let r of customer.rentals) {
+      frequentRenterPoints += calculateFrequentPointers(r);
+    }
+    function calculateFrequentPointers(r) {
+      return (getMovie(r).code === "new" && r.days > 2) ? 2 : 1;
+    }
+    return frequentRenterPoints;
+  }
 }
+const customer = {
+  "name": "martin",
+  "rentals": [
+    { "movieID": "F001", "days": 3 },
+    { "movieID": "F002", "days": 1 },
+  ]
+};
+
+const movies = {
+  "F001": { "title": "Ran", "code": "regular" },
+  "F002": { "title": "Trois Couleurs: Bleu", "code": "regular" },
+}
+
+statement(customer, movies);
